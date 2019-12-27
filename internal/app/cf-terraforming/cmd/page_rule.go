a9bd5dedbc88a965e15e60c6129a8445f4f62a11
chriscf999 <snowdon03@googlemail.com>
2019-12-27 16:07:37 +0000

    Update page_rule.go

--- internal/app/cf-terraforming/cmd/page_rule.go
+++ internal/app/cf-terraforming/cmd/page_rule.go
@@ -12,7 +12,7 @@ import (
 
 const pageRuleTemplate = `
 resource "cloudflare_page_rule" "page_rule_{{.Rule.ID}}" {
-    zone = "{{.Zone.Name}}"
+    zone_id = "{{.Zone.ID}}"
 {{ range .Rule.Targets}}
     target = "{{.Constraint.Value }}"
 {{ end }}

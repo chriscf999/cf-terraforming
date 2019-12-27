7148e21365ddea111283b1fc1782a2f6ee595b4e
chriscf999 <snowdon03@googlemail.com>
2019-12-27 12:22:45 +0000

    Update spectrum_application.go

--- internal/app/cf-terraforming/cmd/spectrum_application.go
+++ internal/app/cf-terraforming/cmd/spectrum_application.go
@@ -69,6 +69,7 @@ var spectrumApplicationCmd = &cobra.Command{
 
 					log.WithFields(logrus.Fields{
 						"ID": app.ID,
+						"ZID": zone.ID,
 					}).Debug("Processing spectrum app")
 
 					if tfstate {
